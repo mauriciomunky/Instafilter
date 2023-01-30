668c0b7cac7d10dacf0fd0573ea5c0d298eb1b88
Maurício Costa <86126994+mauriciomunky@users.noreply.github.com>
2023-01-30 18:42:44 -0300

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Maurício Costa on 30/01/23.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
