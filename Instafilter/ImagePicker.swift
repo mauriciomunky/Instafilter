18ff63083257d2c2da1a0cad2ba861794bcfe45b
Maurício Costa <86126994+mauriciomunky@users.noreply.github.com>
2023-01-30 23:26:54 -0300

    Project Complete

--- Instafilter/ImagePicker.swift
+++ Instafilter/ImagePicker.swift
@@ -1,15 +1,16 @@
 //
-//  ContentView.swift
+//  ImagePicker.swift
 //  Instafilter
 //
 //  Created by Maurício Costa on 30/01/23.
 //
 
-import SwiftUI
 import PhotosUI
+import SwiftUI
 
 struct ImagePicker: UIViewControllerRepresentable {
     @Binding var image: UIImage?
+
     func makeUIViewController(context: Context) -> PHPickerViewController {
         var config = PHPickerConfiguration()
         config.filter = .images
@@ -17,45 +18,32 @@ struct ImagePicker: UIViewControllerRepresentable {
         picker.delegate = context.coordinator
         return picker
     }
-    func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) { }
+
+    func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {
+
+    }
+
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
+
     class Coordinator: NSObject, PHPickerViewControllerDelegate {
+        let parent: ImagePicker
+
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+
         func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
             picker.dismiss(animated: true)
+
             guard let provider = results.first?.itemProvider else { return }
+
             if provider.canLoadObject(ofClass: UIImage.self) {
                 provider.loadObject(ofClass: UIImage.self) { image, _ in
                     self.parent.image = image as? UIImage
                 }
             }
         }
-        
-        var parent: ImagePicker
-        init(_ parent: ImagePicker) {
-            self.parent = parent
-        }
-    }
-}
-
-struct ContentView: View {
-    @State private var image: Image?
-    @State private var showingImagePicker = false
-    var body: some View {
-        VStack {
-            image?.resizable().scaledToFit()
-            Button("Select Image") {
-                showingImagePicker = true
-            }
-        }.sheet(isPresented: $showingImagePicker) {
-            ImagePicker()
-        }
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
     }
 }
