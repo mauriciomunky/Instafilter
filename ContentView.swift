ff28d7c06a1c3f00788897403b52f8788e46381d
Maurício Costa <86126994+mauriciomunky@users.noreply.github.com>
2023-01-30 23:43:58 -0300

    Challenges Complete

--- ContentView.swift
+++ ContentView.swift
@@ -12,6 +12,7 @@ import CoreImage.CIFilterBuiltins
 struct ContentView: View {
     @State private var image: Image?
     @State private var filterIntensity = 0.5
+    @State private var secondaryFilterIntensity = 0.5
     @State private var showingImagePicker = false
     @State private var inputImage: UIImage?
     @State private var currentFilter: CIFilter = CIFilter.sepiaTone()
@@ -29,8 +30,11 @@ struct ContentView: View {
         if inputKeys.contains(kCIInputIntensityKey) {
             currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey)
         }
-        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(filterIntensity * 200, forKey: kCIInputRadiusKey) }
-        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(filterIntensity * 10, forKey: kCIInputScaleKey) }
+        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(secondaryFilterIntensity * 200, forKey: kCIInputRadiusKey) }
+        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(secondaryFilterIntensity * 10, forKey: kCIInputScaleKey) }
+        if inputKeys.contains(kCIInputScaleKey) {
+            currentFilter.setValue(secondaryFilterIntensity * 1500, forKey: kCIInputScaleKey)
+        }
         
         guard let outputImage = currentFilter.outputImage else { return }
         if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
@@ -71,25 +75,40 @@ struct ContentView: View {
                     Slider(value: $filterIntensity).onChange(of: filterIntensity) { _ in
                         applyProcessing()
                     }
-                }.padding(.vertical)
+                }.padding(.top)
+                HStack {
+                    Text("Value")
+                    Slider(value: $secondaryFilterIntensity).onChange(of: secondaryFilterIntensity) { _ in
+                        applyProcessing()
+                    }
+                }.padding(.bottom)
                 HStack {
                     Button("Change filter") {
                         showingFilterSheet = true
                     }
                     Spacer()
-                    Button("Save", action: save)
+                    Button("Save", action: save).disabled(inputImage == nil)
                 }
             }.padding([.horizontal,.bottom]).navigationTitle("Instafilter").onChange(of: inputImage) { _ in loadImage() }.sheet(isPresented: $showingImagePicker) {
-                ImagePicker(image: $inputImage).confirmationDialog("Select a filter", isPresented: $showingFilterSheet) {
-                    Button("Crystallize") { setFilter(CIFilter.crystallize()) }
-                    Button("Edges") { setFilter(CIFilter.edges()) }
-                    Button("Gaussian Blur") { setFilter(CIFilter.gaussianBlur()) }
-                    Button("Pixellate") { setFilter(CIFilter.pixellate()) }
-                    Button("Sepia Tone") { setFilter(CIFilter.sepiaTone()) }
-                    Button("Unsharp Mask") { setFilter(CIFilter.unsharpMask()) }
-                    Button("Vignette") { setFilter(CIFilter.vignette()) }
-                    Button("Cancel", role: .cancel) { }
+                ImagePicker(image: $inputImage)
+            }.confirmationDialog("Select a filter", isPresented: $showingFilterSheet) {
+                Button("Crystallize") { setFilter(CIFilter.crystallize()) }
+                Button("Edges") { setFilter(CIFilter.edges()) }
+                Button("Gaussian Blur") { setFilter(CIFilter.gaussianBlur()) }
+                Button("Pixellate") { setFilter(CIFilter.pixellate()) }
+                Button("Sepia Tone") { setFilter(CIFilter.sepiaTone()) }
+                Button("Unsharp Mask") { setFilter(CIFilter.unsharpMask()) }
+                Button("Vignette") { setFilter(CIFilter.vignette()) }
+                Button("Vortex") {
+                    setFilter(CIFilter.vortexDistortion())
                 }
+                Button("Comic Effect") {
+                    setFilter(CIFilter.comicEffect())
+                }
+                Button("Gloom") {
+                    setFilter(CIFilter.gloom())
+                }
+                Button("Cancel", role: .cancel) { }
             }
         }
     }
